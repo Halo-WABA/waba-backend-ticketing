5414bcaf32e6aa176d84f45ce4a5ea95ac1f4f49
Hee Chan <88177732+HeeChanN@users.noreply.github.com>
2025-04-09 15:26:48 +0900

    [test] : jwtProvider의 resolve, validate 테스트 코드 작성

--- src/test/java/com/festimap/tiketing/global/security/SecurityConfigTest.java
+++ src/test/java/com/festimap/tiketing/global/security/SecurityConfigTest.java
@@ -15,6 +15,7 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -44,5 +45,35 @@ public class SecurityConfigTest {
         mockMvc.perform(get("/api/test/security").header("Authorization", "Bearer " + token))
                 .andExpect(status().isOk())
                 .andExpect(content().string("테스트"));
+    }
+
+    @Test
+    void 올바른_Bearer_접두사가_없는_토큰_테스트() throws Exception {
+        //given
+        String token = jwtProvider.createToken("test", List.of("ROLE_ADMIN"));
+
+        //when & then
+        mockMvc.perform(get("/api/test/security").header("Authorization", "nonBearer " + token))
+                .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    void 잘못된_JWT_구조의_토큰_테스트() throws Exception {
+        // given
+        String invalidJwt = "Bearer just_something_invalid";
+
+        //when & then
+        mockMvc.perform(get("/api/test/security").header("Authorization", invalidJwt))
+                .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    void NULL_토큰_테스트()throws Exception{
+        // given
+        String invalidJwt = "Bearer ";
+
+        //when & then
+        mockMvc.perform(get("/api/test/security").header("Authorization", invalidJwt))
+                .andExpect(status().isUnauthorized());
     }
 }
