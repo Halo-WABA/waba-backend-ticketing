db72f5903b16cf8522f3deb9598f8ee63cecdc4f
Hee Chan <88177732+HeeChanN@users.noreply.github.com>
2025-04-15 03:41:47 +0900

    [feat] : validation 추가

--- src/main/java/com/festimap/tiketing/domain/verification/controller/VerificationController.java
+++ src/main/java/com/festimap/tiketing/domain/verification/controller/VerificationController.java
@@ -4,6 +4,7 @@ import com.festimap.tiketing.domain.verification.dto.VerificationCheckReqDto;
 import com.festimap.tiketing.domain.verification.dto.VerificationReqDto;
 import com.festimap.tiketing.domain.verification.service.VerificationService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -14,12 +15,12 @@ public class VerificationController {
     private final VerificationService verificationService;
 
     @PostMapping("/send/code")
-    public void sendVerificationCode(@RequestBody VerificationReqDto verificationReqDto) {
+    public void sendVerificationCode(@RequestBody @Validated VerificationReqDto verificationReqDto) {
         verificationService.sendVerificationCode(verificationReqDto);
     }
 
     @PostMapping("/check/code")
-    public void checkVerificationCode(@RequestBody VerificationCheckReqDto verificationCheckReqDto) {
+    public void checkVerificationCode(@RequestBody @Validated VerificationCheckReqDto verificationCheckReqDto) {
         verificationService.checkVerificationCode(verificationCheckReqDto);
     }
 }
