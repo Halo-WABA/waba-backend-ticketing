3fec3b6b61d4a58e0bffa28e2e630d7508922cec
Hee Chan <88177732+HeeChanN@users.noreply.github.com>
2025-04-18 04:05:53 +0900

    [refactor] : uri 권한 수정

--- src/main/java/com/festimap/tiketing/global/config/security/AuthorizationConfig.java
+++ src/main/java/com/festimap/tiketing/global/config/security/AuthorizationConfig.java
@@ -2,6 +2,7 @@ package com.festimap.tiketing.global.config.security;
 
 import com.festimap.tiketing.global.constants.SecurityConstants;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 
 @Configuration
@@ -10,8 +11,9 @@ public class AuthorizationConfig {
     public void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers(SecurityConstants.SWAGGER_URLS).permitAll()
-                .antMatchers(SecurityConstants.PUBLIC_POST_URLS).permitAll()
-                .antMatchers(SecurityConstants.PUBLIC_GET_URLS).permitAll()
+                .antMatchers(HttpMethod.POST, SecurityConstants.PUBLIC_POST_URLS).permitAll()
+                .antMatchers(HttpMethod.GET, SecurityConstants.PUBLIC_GET_URLS).permitAll()
+                .antMatchers(HttpMethod.GET, SecurityConstants.ACTUATOR_URL).permitAll()
                 .anyRequest().authenticated();
     }
 }
